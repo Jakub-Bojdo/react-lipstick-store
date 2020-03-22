5a99679a6db1344ca2ea6ec97fc3c141d35d876e
Jakub Bojdo <krystianfadej@gmail.com>
2020-03-22 23:44:14 +0100

    Updating Heading.js in atoms

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Heading = styled.h1`
   font-size: ${({ theme, big }) =>
-    big ? theme.fontSize.xl : theme.fontSize.l};
+    big ? theme.fontSize.xxl : theme.fontSize.l};
   font-weight: ${({ theme }) => theme.bold};
 `;
 
