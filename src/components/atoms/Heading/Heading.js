e4ee385db1f2586e7543c12c664ccaa609042705
Jakub Bojdo <krystianfadej@gmail.com>
2020-04-12 23:04:40 +0200

    Adding media queries to Heading.js

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -4,6 +4,10 @@ const Heading = styled.h1`
   font-size: ${({ theme, big }) =>
     big ? theme.fontSize.xxl : theme.fontSize.l};
   font-weight: ${({ theme }) => theme.bold};
+
+  @media (max-width: 900px) {
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
 `;
 
 export default Heading;
