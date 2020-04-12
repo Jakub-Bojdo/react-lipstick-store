f0ca7f52ff162a66173ccb3674db463242083219
Jakub Bojdo <krystianfadej@gmail.com>
2020-04-12 23:26:48 +0200

    Adding new color and two new fonts to mainTheme.js

--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -3,7 +3,7 @@ export const mainTheme = {
   ocean: "rgb(138,254,254)",
   green: "rgb(158,255,183)",
   orange: "rgb(242,216,165)",
-  pink: "rgb(253, 198, 221)",
+  rose: "rgb(253, 198, 221)",
   lighterPink: "rgb(245,215,223)",
   darkerPink: "rgb(214, 65, 97)",
   red: "rgb(199, 0, 57)",
@@ -11,6 +11,7 @@ export const mainTheme = {
   grey100: "hsl(0, 0%, 96%)",
   grey200: "hsl(0, 0%, 88%)",
   grey300: "hsl(0, 0%, 60%)",
+  grey400: "hsl(0, 0%, 40%)",
   black: "rgb(0,0,0)",
   light: 300,
   medium: 400,
@@ -19,11 +20,12 @@ export const mainTheme = {
   fontSize: {
     xxs: "1rem",
     xs: "1.2rem",
-    cart: "1.4rem",
+    cart: "1.5rem",
     s: "1.6rem",
+    p: "1.7rem",
     m: "2.1rem",
     l: "2.4rem",
     xl: "3.2rem",
-    xxl: "5rem"
-  }
+    xxl: "5rem",
+  },
 };
